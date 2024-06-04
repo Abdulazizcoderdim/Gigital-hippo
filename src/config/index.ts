9f152871be900755cbefa695d0c287c3704b20e6
AbdulazizRus <coderdim@gmail.com>
2024-06-04 22:31:43 +0500

    chas

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: 'UI Kits',
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: 'Editor picks',
+                href: '#',
+                imageSrc: '/nav/ui-kits/mixed/jpg'
+            }
+        ]
+    }
+]
+
+//55:00
