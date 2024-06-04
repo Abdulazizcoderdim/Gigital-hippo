6d8b1295ec678f296bd5eb0c2aeb0ca87ea69121
AbdulazizRus <coderdim@gmail.com>
2024-06-04 13:05:50 +0500

    sas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <p>hello world</p>
+    <MaxWidthWrapper>
+      <div className="py-20 items-center max-w-3xl mx-auto text-center flex flex-col">
+          as
+      </div>
+    </MaxWidthWrapper>
   );
 }
+//23:00
