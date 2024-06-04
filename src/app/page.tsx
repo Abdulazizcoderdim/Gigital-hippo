58dabd77e02c7e4913c4ee3aa9a540de99741c21
AbdulazizRus <coderdim@gmail.com>
2024-06-04 13:19:27 +0500

    as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 items-center max-w-3xl mx-auto text-center flex flex-col">
-          as
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality{' '}
+          <span className="text-blue-600">digital assets</span>.
+        </h1>
+        <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
+          Welcome to DigitalHippo. Every asset on our platform is verified by
+          our team to ensure our highest quality standards.
+        </p>
+        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+          <Link className={buttonVariants()} href={'/products'}>Browse Trending</Link>
+        </div>
       </div>
     </MaxWidthWrapper>
-  );
+  )
 }
-//23:00
+//30:00
