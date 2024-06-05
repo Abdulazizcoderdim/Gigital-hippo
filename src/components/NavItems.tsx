ac69e236f9ca9742b6be25d3bc5ca94a3b7b2318
AbdulazizRus <coderdim@gmail.com>
2024-06-05 09:15:53 +0500

    asdasd

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,17 +1,42 @@
-"use client"
+'use client'
 
-import { useState } from "react"
+import { PRODUCT_CATEGORIES } from '@/config'
+import { useState,useRef } from 'react'
+import NavItem from './NavItem'
 
 const NavItems = () => {
-    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+  const [activeIndex, setActiveIndex] = useState<null | number>(null)
 
-    
+  const isAnyOpen = activeIndex !== null
+
+  const navRef = useRef<HTMLDivElement | null>(null)
 
   return (
     <div className="flex gap-4 h-full">
-        
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null)
+          } else {
+            setActiveIndex(i)
+          }
+        }
+
+        const isOpen = i === activeIndex
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        )
+      })}
     </div>
   )
 }
 
 export default NavItems
+//1:17:00
