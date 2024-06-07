7e402dc86dbe3f7579e359b936ee61c0902ffe3c
AbdulazizRus <coderdim@gmail.com>
2024-06-07 15:51:53 +0500

    changed

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,14 @@ const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
 const start = async () => {
-    const payload = await getPayloadClient()
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+      },
+    },
+  })
 }
 
 start()
