28e97ae6ed44c329f0f4015a0408c9e799c8327c
AbdulazizRus <coderdim@gmail.com>
2024-06-07 13:56:02 +0500

    xzzxzx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,15 +4,19 @@ import { ShoppingCart } from 'lucide-react'
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from './ui/sheet'
 import { Separator } from './ui/separator'
 import { formatPrice } from '@/lib/utils'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import Image from 'next/image'
 
 const Cart = () => {
-  const itemCount = 1
+  const itemCount = 0
 
   const fee = 0
 
@@ -45,11 +49,48 @@ const Cart = () => {
                   <span className="flex-1">Transaction Free</span>
                   <span>{formatPrice(fee)}</span>
                 </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
               </div>
+
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href={'/cart'}
+                    className={buttonVariants({
+                      className: 'w-full',
+                    })}
+                  >
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image fill alt="empty cart" src="/hippo-empty-cart.png"></Image>
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: 'link',
+                  size: 'sm',
+                  className: 'text-sm text-muted-foreground',
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
---
+++ src/get-payload.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+})
+
+
+
+export const getPayloadClient = async () => {}
+
+
+//2:01:00
